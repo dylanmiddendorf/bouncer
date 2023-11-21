305bc31db0286b1d19bf4caa02bd46d63903dcb9
dylanmiddendorf <middendorf.dylan@gmail.com>
2023-11-21 15:20:07 -0600

    Update terminology for clarification

--- bouncer/modules/verification.py
+++ bouncer/modules/verification.py
@@ -11,3 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from discord import Interaction
+from modules import Module
+
+
+class Verificiation(Module):
+    async def verification_create(self, interaction: Interaction) -> None:
+        pass
